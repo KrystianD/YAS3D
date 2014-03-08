16aa26549bf115c1722590ed952a1d82c61a4056
Krystian Dużyński <krystekd@gmail.com>
2014-03-09 00:45:19 +0100

    added snake moving and world rotation

--- TG_Project-android/src/pl/jakd/tg_project/MainActivity.java
+++ TG_Project-android/src/pl/jakd/tg_project/MainActivity.java
@@ -1,20 +1,24 @@
 package pl.jakd.tg_project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useAccelerometer = true;
-        cfg.useCompass = true;
-        cfg.useImmersiveMode = true;
-        
-        initialize(new TGProject(), cfg);
-    }
+public class MainActivity extends AndroidApplication
+{
+	@Override
+	public void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate (savedInstanceState);
+		Log.d ("KD", "AS1D");
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration ();
+		cfg.useAccelerometer = true;
+		cfg.useCompass = true;
+		cfg.useImmersiveMode = true;
+		Log.d ("KD", "ASD");
+		initialize (new TGProject (this), cfg);
+	}
 }
---
+++ TG_Project-android/src/pl/jakd/tg_project/TGProject.java
@@ -0,0 +1,392 @@
+package pl.jakd.tg_project;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Timer;
+
+public class TGProject implements ApplicationListener, SensorEventListener, InputProcessor
+{
+	public PerspectiveCamera cam;
+	public ModelBatch modelBatch;
+	public Model model[] = new Model[10];
+	public ModelInstance instance[] = new ModelInstance[10];
+	public Model model2, modelSnakePart;
+	public ModelInstance instance2, instSnakePart;
+	public Environment environment;
+	public CameraInputController camController;
+	public BitmapFont font;
+	public SpriteBatch batch;
+
+	private Quaternion quat = new Quaternion ();
+
+	private Context ctx;
+	private SensorManager mSensorManager;
+	private Sensor mAccel, mGyro, mMagnet;
+
+	float aX = 0, aY = 0, aZ = 0, gX = 0, gY = 0, gZ = 0, mX = 0, mY = 0, mZ = 0;
+	Boolean hasA = false, hasG = false, hasM = false;
+	Mad mad;
+
+	public TGProject (Context ctx)
+	{
+		this.ctx = ctx;
+
+		mad = new Mad ();
+		//System.loadLibrary ("mad");
+	}
+
+	Vector3 pt[] = new Vector3[100];
+
+	Vector3 dir = new Vector3 (0, 0, 1);
+
+	@Override
+	public void create ()
+	{
+		dir.nor ();
+		font = new BitmapFont ();
+		batch = new SpriteBatch ();
+		modelBatch = new ModelBatch ();
+
+		cam = new PerspectiveCamera (60, Gdx.graphics.getWidth (), Gdx.graphics.getHeight ());
+		cam.position.set (3f, 3f, 3f);
+		cam.position.set (0f, 0f, 30f);
+		cam.position.set (0f, 0f, 0f);
+		cam.lookAt (0, 0, 0);
+		cam.near = 0.1f;
+		cam.far = 300f;
+		cam.update ();
+
+		camController = new CameraInputController (cam);
+		Gdx.input.setInputProcessor (camController);
+		Gdx.input.setInputProcessor (this);
+
+		ModelBuilder modelBuilder = new ModelBuilder ();
+		model[0] = modelBuilder.createBox (1f, 1f, 5f, new Material (ColorAttribute.createDiffuse (Color.RED)), Usage.Position | Usage.Normal);
+		instance[0] = new ModelInstance (model[0]);
+
+		model[1] = modelBuilder.createBox (1f, 5f, 1f, new Material (ColorAttribute.createDiffuse (Color.GREEN)), Usage.Position | Usage.Normal);
+		instance[1] = new ModelInstance (model[1]);
+
+		model[2] = modelBuilder.createBox (5f, 1f, 1f, new Material (ColorAttribute.createDiffuse (Color.BLUE)), Usage.Position | Usage.Normal);
+		instance[2] = new ModelInstance (model[2]);
+
+		//model2 = modelBuilder.createBox (2f, 2f, 2f, new Material (ColorAttribute.createDiffuse (Color.RED)), Usage.Position | Usage.Normal);
+		//instance2 = new ModelInstance (model2);
+
+		model2 = modelBuilder.createSphere (
+				1, 1, 1, 10, 10,
+				new Material (ColorAttribute.createDiffuse (Color.RED)), Usage.Position | Usage.Normal);
+		instance2 = new ModelInstance (model2);
+
+		modelSnakePart = modelBuilder.createSphere (
+				1, 1, 1, 5, 5,
+				new Material (ColorAttribute.createDiffuse (Color.RED)), Usage.Position | Usage.Normal);
+		instSnakePart = new ModelInstance (modelSnakePart);
+
+		environment = new Environment ();
+		environment.set (new ColorAttribute (ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add (new DirectionalLight ().set (0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		mSensorManager = (SensorManager)ctx.getSystemService (Context.SENSOR_SERVICE);
+		mAccel = mSensorManager.getDefaultSensor (Sensor.TYPE_ACCELEROMETER);
+		mGyro = mSensorManager.getDefaultSensor (Sensor.TYPE_GYROSCOPE);
+		mMagnet = mSensorManager.getDefaultSensor (Sensor.TYPE_MAGNETIC_FIELD);
+
+		mSensorManager.registerListener (this, mAccel, SensorManager.SENSOR_DELAY_FASTEST);
+		mSensorManager.registerListener (this, mGyro, SensorManager.SENSOR_DELAY_FASTEST);
+		mSensorManager.registerListener (this, mMagnet, SensorManager.SENSOR_DELAY_FASTEST);
+
+		Random r = new Random ();
+
+		for (int i = 0; i < 100; i++)
+		{
+			pt[i] = new Vector3 (r.nextFloat () * 2 - 1, r.nextFloat () * 2 - 1, r.nextFloat () * 2 - 1);
+			pt[i] = pt[i].nor ();
+			pt[i] = pt[i].mul (20);
+		}
+
+		Timer.schedule (new Timer.Task ()
+		{
+
+			@Override
+			public void run ()
+			{
+				calc ();
+
+			}
+		}, 0.02f, 0.02f);
+	}
+	@Override
+	public void dispose ()
+	{
+		modelBatch.dispose ();
+	}
+
+	@Override
+	public void pause ()
+	{
+
+	}
+
+	Vector2 latlongToMeters (Vector2 pos)
+	{
+		float longtitude = pos.y;
+		float latitude = pos.x / (float)Math.cos (longtitude);
+		return new Vector2 (latitude, longtitude);
+	}
+
+	float x = 0;
+	Vector3 snakePos = new Vector3 (1, 0, 0);
+	ArrayList<Vector3> points = new ArrayList<Vector3> ();
+	float snakeAng = 0, snakeAngInc = 0;
+
+	@Override
+	public void render ()
+	{
+		//mad (1, 1, 1, 2, 2, 2, 3, 3, 3);
+		Gdx.gl.glViewport (0, 0, Gdx.graphics.getWidth (), Gdx.graphics.getHeight ());
+		Gdx.gl.glClear (GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		quat.set (mad.q1, mad.q2, mad.q3, -mad.q0);
+
+		batch.begin ();
+		font.drawMultiLine (batch, getOrientationString (), 20, Gdx.graphics.getHeight () - 10);
+		batch.end ();
+
+		modelBatch.begin (cam);
+
+		for (int i = 0; i < 3; i++)
+		{
+			instance[i].transform.idt ();
+			instance[i].transform.scl (0.3f);
+			instance[i].transform.rotate (quat);
+			//modelBatch.render (instance[i], environment);
+		}
+
+		for (int i = 0; i < 1; i++)
+		{
+			Vector3 snakePosNorm = snakePos;
+			snakePosNorm.nor ();
+
+			dir.rotateRad (snakePosNorm, snakeAngInc * 5);
+
+			Vector3 dir2 = new Vector3 (dir).mul (0.005f);
+			Vector3 newPt = new Vector3 (snakePos).add (dir2);
+			newPt.nor ();
+
+			dir = new Vector3 (newPt).sub (snakePos);
+			dir.nor ();
+
+			snakePos = newPt;
+			points.add (snakePos);
+			if (points.size () > 100)
+				points.remove (0);
+		}
+
+		//Quaternion q = new Quaternion ();
+		//q.setFromAxisRad (snakePosNorm, 0.1f);
+
+		Matrix3 a = new Matrix3 ();
+
+		for (int i = 0; i < points.size (); i++)
+		{
+			instSnakePart.transform.idt ();
+			instSnakePart.transform.rotate (quat);
+			instSnakePart.transform.translate (points.get (i).x * 15, points.get (i).y * 15, points.get (i).z * 15);
+
+			//instSnakePart.transform.scale (1, 1.0f / 5.0f, 1);
+			modelBatch.render (instSnakePart, environment);
+		}
+
+		/*Vector3 ang = latlongToMeters (new Vector2 (x, 0.3f));
+
+		instance[1].transform.idt ();
+		instance[1].transform.rotate (quat);
+		instance[1].transform.rotateRad (0, 0, -1, ang.x);
+		instance[1].transform.rotateRad (0, 1, 0, ang.y);
+		instance[1].transform.translate (-15f, 0, 0);
+		modelBatch.render (instance[1], environment);*/
+
+		/*instance.transform.idt ();
+		instance.transform.rotate (quat);
+		modelBatch.render (instance, environment);
+		instance2.transform.idt ();
+		instance2.transform.rotate (quat);
+		instance2.transform.translate (0, 0, 1.5f);
+		modelBatch.render (instance2, environment);*/
+		for (int i = 0; i < 100; i++)
+		{
+			instance2.transform.idt ();
+			instance2.transform.rotate (quat);
+			instance2.transform.translate (pt[i]);
+			modelBatch.render (instance2, environment);
+
+		}
+		modelBatch.end ();
+	}
+	@Override
+	public void resize (int width, int height)
+	{
+
+	}
+
+	@Override
+	public void resume ()
+	{
+
+	}
+
+	private String getOrientationString ()
+	{
+		StringBuilder builder = new StringBuilder ();
+		builder.append ("\nazimuth: ");
+		builder.append ((int)Gdx.input.getAzimuth ());
+		builder.append ("\npitch: ");
+		builder.append ((int)Gdx.input.getPitch ());
+		builder.append ("\nroll: ");
+		builder.append ((int)Gdx.input.getRoll ());
+		return builder.toString ();
+	}
+
+	@Override
+	public void onAccuracyChanged (Sensor sensor, int accuracy)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	long lastCalc = 0;
+
+	int xMax = -99999;
+	int xMin = 99999;
+
+	@Override
+	public void onSensorChanged (SensorEvent event)
+	{
+		if (event.sensor.getType () == Sensor.TYPE_ACCELEROMETER)
+		{
+			aX = event.values[1];
+			aY = -event.values[0];
+			aZ = -event.values[2];
+			hasA = true;
+		}
+		if (event.sensor.getType () == Sensor.TYPE_GYROSCOPE)
+		{
+			gX = -event.values[1];
+			gY = event.values[0];
+			gZ = event.values[2];
+			hasG = true;
+		}
+		if (event.sensor.getType () == Sensor.TYPE_MAGNETIC_FIELD)
+		{
+			mX = -event.values[1];
+			mY = event.values[0];
+			mZ = event.values[2];
+			hasM = true;
+		}
+
+	}
+
+	public void calc ()
+	{
+		if (hasA && hasM)
+		{
+			long ticks = System.currentTimeMillis ();
+			if (lastCalc == 0)
+				lastCalc = ticks;
+			float diff = (float)(ticks - lastCalc) / 1000.0f;
+			lastCalc = ticks;
+
+			//Log.d ("KD",
+			//		String.format ("ax %10f, ay %10f, az %10f, mx %10f, my %10f, mz %10f, gx %10f, gy %10f, gz %10f", aX, aY, aZ, mX, mY, mZ, gX, gY, gZ));
+
+			mad.MadgwickAHRSupdate (gX, gY, gZ, aX, aY, aZ, mX, mY, mZ, 0.02f);
+			gX = gY = gZ = 0;
+		}
+	}
+	@Override
+	public boolean keyDown (int arg0)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean keyTyped (char arg0)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean keyUp (int arg0)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean mouseMoved (int arg0, int arg1)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean scrolled (int arg0)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean touchDown (int arg0, int arg1, int arg2, int arg3)
+	{
+		Log.d ("KD", "d" + arg0);
+		if (arg0 < Gdx.app.getGraphics ().getWidth () / 2)
+			snakeAngInc = -0.01f;
+		else
+			snakeAngInc = 0.01f;
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean touchDragged (int arg0, int arg1, int arg2)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean touchUp (int arg0, int arg1, int arg2, int arg3)
+	{
+		Log.d ("KD", "u");
+		snakeAngInc = 0f;
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
