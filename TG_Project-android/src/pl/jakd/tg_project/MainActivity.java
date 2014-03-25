649336b785edd78a43fa2f8ca895ee85255240f4
Jacek Andrzejewski <jacaato@gmail.com>
2014-03-25 22:07:24 +0100

    main menu added

--- TG_Project-android/src/pl/jakd/tg_project/MainActivity.java
+++ TG_Project-android/src/pl/jakd/tg_project/MainActivity.java
@@ -3,6 +3,7 @@ package pl.jakd.tg_project;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
@@ -12,13 +13,14 @@ public class MainActivity extends AndroidApplication
 	public void onCreate (Bundle savedInstanceState)
 	{
 		super.onCreate (savedInstanceState);
-		Log.d ("KD", "AS1D");
 
 		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration ();
 		cfg.useAccelerometer = true;
 		cfg.useCompass = true;
 		cfg.useImmersiveMode = true;
-		Log.d ("KD", "ASD");
-		initialize (new TGProject (this), cfg);
+		
+		ApplicationListener listener = new GameSnake(this);
+		initialize(listener,cfg);
+		//initialize (new TGProject (this), cfg);
 	}
 }
